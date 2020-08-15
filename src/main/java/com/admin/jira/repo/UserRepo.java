d2273d83613f0356f52e0e20a7862dcf4a626a8a
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-08-15 00:14:59 -0400

    add annotation

--- src/main/java/com/admin/jira/repo/UserRepo.java
+++ src/main/java/com/admin/jira/repo/UserRepo.java
@@ -1,10 +1,11 @@
 package com.admin.jira.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.stereotype.Repository;
 
 import com.admin.jira.entity.User;
 
+@Repository
 //@CrossOrigin("http://localhost:4200")
 public interface UserRepo extends JpaRepository<User, Integer> {
 
